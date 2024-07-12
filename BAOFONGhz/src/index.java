db23c0ce194cb5551a7e9cde4855da3ea99bdb5a
Lorena <manuelgarciabaceiredo@gmail.com>
2024-07-12 21:59:15 +0200

    Add files via upload

--- BAOFONGhz/src/index.java
+++ BAOFONGhz/src/index.java
@@ -44,6 +44,8 @@ public class index extends javax.swing.JFrame {
         jTextField5 = new javax.swing.JTextField();
         jTextField6 = new javax.swing.JTextField();
         jComboBox2 = new javax.swing.JComboBox<>();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Baofonghz");
@@ -110,7 +112,7 @@ public class index extends javax.swing.JFrame {
         });
 
         jLabel3.setForeground(new java.awt.Color(153, 153, 153));
-        jLabel3.setText("BAOFONGhz™ - v. 0.4.12072024");
+        jLabel3.setText("BAOFONGhz™ - v. 0.5.12072024");
 
         jComboBox1.setToolTipText("Listening list");
 
@@ -135,6 +137,22 @@ public class index extends javax.swing.JFrame {
 
         jComboBox2.setToolTipText("Broadcasting list");
 
+        jButton3.setText("x");
+        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("x");
+        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -144,39 +162,44 @@ public class index extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
-                .addGap(31, 31, 31)
+                .addGap(81, 81, 81)
+                .addComponent(jLabel4)
+                .addContainerGap(81, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(50, 50, 50)
-                        .addComponent(jLabel4))
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton3)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(12, 12, 12)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addGroup(layout.createSequentialGroup()
-                                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(12, 12, 12)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addGroup(layout.createSequentialGroup()
                                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-                .addContainerGap(37, Short.MAX_VALUE))
+                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton4))
+                            .addComponent(jLabel1))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -190,7 +213,9 @@ public class index extends javax.swing.JFrame {
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1)
                 .addGap(18, 18, 18)
@@ -204,10 +229,12 @@ public class index extends javax.swing.JFrame {
                     .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addComponent(jLabel3)
                 .addContainerGap())
         );
@@ -218,14 +245,16 @@ public class index extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         
+        jButton1.setBackground(java.awt.Color.RED);
+        
         String SERVER_ADDRESS = jTextField1.getText();
         String puerto = jTextField2.getText();
-        String message = "Sintonizando:\n "+SERVER_ADDRESS+":"+puerto;
+        String message = "Sintonizando\n "+SERVER_ADDRESS+":"+puerto;
         JOptionPane.showMessageDialog(null, message, "BAOFONGhz", JOptionPane.INFORMATION_MESSAGE);
         
         
         jLabel1.setText("Client: Connected... ");
-        //jComboBox1.addItem(jTextField1.getText()+":"+jTextField2.getText());
+        //añade a la ultima posición // jComboBox1.addItem(jTextField1.getText()+":"+jTextField2.getText());
         String newItem = jTextField1.getText()+":"+jTextField2.getText();
         /*** Añadir en primera posición **/
             // Obtener el número de elementos actuales
@@ -263,20 +292,20 @@ public class index extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         
-        
+        jButton2.setBackground(java.awt.Color.RED);
         //String ip_SERVER_ADDRESS = jTextField3.getText();
         int PORT = Integer.parseInt(jTextField4.getText());
         
         ip ip = new ip();
         
-        String message = "Transmitiendo:\n "+ip.getIpLocal()+":"+jTextField4.getText()+" ==> "+ip.getIpPublic()+":"+jTextField6.getText();
+        String message = "Transmitiendo\n Local: "+ip.getIpLocal()+":"+jTextField4.getText()+" \n Net: "+ip.getIpPublic()+":"+jTextField6.getText();
         JOptionPane.showMessageDialog(null, message, "BAOFONGhz", JOptionPane.INFORMATION_MESSAGE);
        
         
        jLabel2.setText("Server: Broadcasting...");
        jTextField3.setText(ip.getIpLocal());
        jTextField5.setText(ip.getIpPublic());
-       //jComboBox2.addItem(jTextField5.getText()+":"+jTextField6.getText()+" ("+jTextField4.getText()+")");
+       //añade a la ultima posición // jComboBox2.addItem(jTextField5.getText()+":"+jTextField6.getText()+" ("+jTextField4.getText()+")");
        String newItem = jTextField5.getText()+":"+jTextField6.getText()+" ("+jTextField4.getText()+")";
         /*** Añadir en primera posición **/
             // Obtener el número de elementos actuales
@@ -298,15 +327,17 @@ public class index extends javax.swing.JFrame {
         /*** ***/
         
         servidor server = new servidor();
-       // server.VoiceChatServer(PORT);
+        /*
         Thread serverThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                server.VoiceChatServer(PORT);
-            }
-        });
-        serverThread.start();
-        
+             @Override
+             public void run() {
+                 server.VoiceChatServer(PORT);
+             }
+         });
+         serverThread.start();
+         */
+        server.VoiceChatServer(PORT); // clase actualizada a multihilo
+       
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
@@ -332,6 +363,28 @@ public class index extends javax.swing.JFrame {
     private void jTextField6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField6ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField6ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        
+        
+        jComboBox1.removeAllItems();
+        jButton1.setBackground(java.awt.Color.GRAY);
+        cliente client = new cliente();
+        client.stop();
+        
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        
+        
+        jComboBox2.removeAllItems();
+        jButton2.setBackground(java.awt.Color.GRAY);
+        servidor server = new servidor();
+        server.stop();
+        
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -379,6 +432,8 @@ public class index extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
--- BAOFONGhz/src/servidor.java
+++ BAOFONGhz/src/servidor.java
@@ -17,34 +17,94 @@ public class servidor {
     //private static final int PORT = 12345;
     private static final int BUFFER_SIZE = 4096;
 
+    private volatile boolean running = true;
+    private volatile boolean paused = false;
+    private final Object pauseLock = new Object();
+    private Thread serverThread;
+
     public void VoiceChatServer(int PORT) {
-        System.out.println("Servidor de chat de voz iniciado...");
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Esperando conexiones...");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente conectado");
+        serverThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Servidor de chat de voz iniciado...");
+                try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                    System.out.println("Esperando conexiones...");
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Cliente conectado");
 
-            AudioFormat format = new AudioFormat(44100, 16, 2, true, true);
-            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-            TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(info);
-            microphone.open(format);
-            microphone.start();
+                    AudioFormat format = new AudioFormat(44100, 16, 2, true, true);
+                    DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+                    TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(info);
+                    microphone.open(format);
+                    microphone.start();
 
-            OutputStream out = clientSocket.getOutputStream();
+                    OutputStream out = clientSocket.getOutputStream();
+                    byte[] buffer = new byte[BUFFER_SIZE];
 
-            byte[] buffer = new byte[BUFFER_SIZE];
-
-            System.out.println("Transmitiendo audio...");
-            while (true) {
-                int bytesRead = microphone.read(buffer, 0, buffer.length);
-                if (bytesRead > 0) {
-                    out.write(buffer, 0, bytesRead);
+                    System.out.println("Transmitiendo audio...");
+                    while (running) {
+                        synchronized (pauseLock) {
+                            while (paused) {
+                                try {
+                                    pauseLock.wait();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        // Tu código para leer y enviar audio
+                        int bytesRead = microphone.read(buffer, 0, buffer.length);
+                        if (bytesRead > 0) {
+                            out.write(buffer, 0, bytesRead);
+                        }
+                    }
+                    microphone.close();
+                } catch (IOException | LineUnavailableException e) {
+                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Fatal error VoIP", "BAOFONGhz", JOptionPane.ERROR_MESSAGE);
                 }
             }
-        } catch (IOException | LineUnavailableException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Fatal error VoIP", "BAOFONGhz", JOptionPane.ERROR_MESSAGE);
-            
+        });
+        serverThread.start();
+    }
+
+    public void stop() {
+        running = false;
+        serverThread.interrupt();
+        System.out.println("Servidor detenido.");
+    }
+
+    public void pause() {
+        paused = true;
+        System.out.println("Servidor pausado.");
+    }
+
+    public void resume() {
+        synchronized (pauseLock) {
+            paused = false;
+            pauseLock.notifyAll();
+            System.out.println("Servidor reanudado.");
         }
     }
+
+    
+    
+    /***
+     * Puedes pausar y reanudar el servidor de chat de voz llamando a los métodos pause() y resume():
+     * 
+        VoiceChatServer server = new VoiceChatServer();
+        server.VoiceChatServer(12345);
+
+        // Para pausar el servidor
+        server.pause();
+
+        // Para reanudar el servidor
+        server.resume();
+
+        // Para detener el servidor
+        server.stop();
+     * 
+     */
+    
+    
 }
